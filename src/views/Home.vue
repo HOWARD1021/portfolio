55688887a2f6041b380e216dbf67d359b7936e0e
aquaHsu <bellems811@gmail.com>
2022-05-31 18:38:03 +0800

    btn hover effect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -452,7 +452,7 @@
       <div>
         <router-link  to="/sideProject" 
                       tag="button"
-                      class="btn hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      class="btn bg-blue-100 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
         >
           Go to Project
         </router-link>
@@ -547,7 +547,7 @@ $color-fox: #e37c67;
     }
 
     .btn{
-      @apply inline-flex justify-center rounded-md border border-transparent bg-blue-100 mt-3 px-4 py-2 text-sm font-medium text-blue-900;
+      @apply inline-flex justify-center rounded-md border border-transparent  mt-3 px-4 py-2 text-sm font-medium text-blue-900;
       
     }
   }
